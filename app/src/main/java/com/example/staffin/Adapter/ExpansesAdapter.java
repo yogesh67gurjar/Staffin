e71c2e75464b04dfecffa4619d4ecf6e80583c68
yogesh gurjar <yogesh67gurjar@gmail.com>
2023-05-26 15:03:59 +0530

    attendance

--- app/src/main/java/com/example/staffin/Adapter/ExpansesAdapter.java
+++ app/src/main/java/com/example/staffin/Adapter/ExpansesAdapter.java
@@ -16,15 +16,16 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.example.staffin.R;
 import com.example.staffin.Response.AllExpenses;
 import com.example.staffin.Response.AllPayroll;
+import com.example.staffin.Response.Expenses;
 
 import java.util.List;
 
 public class ExpansesAdapter extends RecyclerView.Adapter<ExpansesAdapter.MyViewHolder> {
 
     Context context;
-    List<AllExpenses.GetAllExpenseDetail> resp;
+    List<Expenses.GetAllExpenseDetail> resp;
 
-    public ExpansesAdapter(Context context, List<AllExpenses.GetAllExpenseDetail> resp) {
+    public ExpansesAdapter(Context context, List<Expenses.GetAllExpenseDetail> resp) {
         this.context = context;
         this.resp = resp;
     }
@@ -39,7 +40,7 @@ public class ExpansesAdapter extends RecyclerView.Adapter<ExpansesAdapter.MyView
 
     @Override
     public void onBindViewHolder(@NonNull ExpansesAdapter.MyViewHolder holder, int position) {
-        AllExpenses.GetAllExpenseDetail singleUnit = resp.get(position);
+        Expenses.GetAllExpenseDetail singleUnit = resp.get(position);
         Animation animationLeft = AnimationUtils.loadAnimation(holder.mainConstraint.getContext(), android.R.anim.slide_in_left);
         holder.mainConstraint.startAnimation(animationLeft);
         holder.txtEmpId.startAnimation(animationLeft);
@@ -59,7 +60,7 @@ public class ExpansesAdapter extends RecyclerView.Adapter<ExpansesAdapter.MyView
         return resp.size();
     }
 
-    public class MyViewHolder extends RecyclerView.ViewHolder {
+    public static class MyViewHolder extends RecyclerView.ViewHolder {
         ConstraintLayout mainConstraint;
         TextView txtEmpId, txtItemName, txtPrice;
         ImageButton btnDownload;
---
+++ app/src/main/java/com/example/staffin/Response/Expenses.java
@@ -0,0 +1,545 @@
+package com.example.staffin.Response;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class Expenses {
+    @SerializedName("status")
+    @Expose
+    private Integer status;
+    @SerializedName("message")
+    @Expose
+    private String message;
+    @SerializedName("get_all_expense_details")
+    @Expose
+    private List<GetAllExpenseDetail> getAllExpenseDetails;
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public List<GetAllExpenseDetail> getGetAllExpenseDetails() {
+        return getAllExpenseDetails;
+    }
+
+    public void setGetAllExpenseDetails(List<GetAllExpenseDetail> getAllExpenseDetails) {
+        this.getAllExpenseDetails = getAllExpenseDetails;
+    }
+
+    public static class GetAllExpenseDetail {
+        @SerializedName("id")
+        @Expose
+        private Integer id;
+        @SerializedName("item_name")
+        @Expose
+        private String itemName;
+        @SerializedName("slug")
+        @Expose
+        private String slug;
+        @SerializedName("purchase_date")
+        @Expose
+        private String purchaseDate;
+        @SerializedName("purchase_from")
+        @Expose
+        private String purchaseFrom;
+        @SerializedName("price")
+        @Expose
+        private Integer price;
+        @SerializedName("bill")
+        @Expose
+        private String bill;
+        @SerializedName("image_bill")
+        @Expose
+        private List<String> imageBill;
+        @SerializedName("company_id")
+        @Expose
+        private Integer companyId;
+        @SerializedName("employee_id")
+        @Expose
+        private List<EmployeeId> employeeId;
+        @SerializedName("status")
+        @Expose
+        private String status;
+        @SerializedName("bill_url")
+        @Expose
+        private String billUrl;
+
+        public Integer getId() {
+            return id;
+        }
+
+        public void setId(Integer id) {
+            this.id = id;
+        }
+
+        public String getItemName() {
+            return itemName;
+        }
+
+        public void setItemName(String itemName) {
+            this.itemName = itemName;
+        }
+
+        public String getSlug() {
+            return slug;
+        }
+
+        public void setSlug(String slug) {
+            this.slug = slug;
+        }
+
+        public String getPurchaseDate() {
+            return purchaseDate;
+        }
+
+        public void setPurchaseDate(String purchaseDate) {
+            this.purchaseDate = purchaseDate;
+        }
+
+        public String getPurchaseFrom() {
+            return purchaseFrom;
+        }
+
+        public void setPurchaseFrom(String purchaseFrom) {
+            this.purchaseFrom = purchaseFrom;
+        }
+
+        public Integer getPrice() {
+            return price;
+        }
+
+        public void setPrice(Integer price) {
+            this.price = price;
+        }
+
+        public String getBill() {
+            return bill;
+        }
+
+        public void setBill(String bill) {
+            this.bill = bill;
+        }
+
+        public List<String> getImageBill() {
+            return imageBill;
+        }
+
+        public void setImageBill(List<String> imageBill) {
+            this.imageBill = imageBill;
+        }
+
+        public Integer getCompanyId() {
+            return companyId;
+        }
+
+        public void setCompanyId(Integer companyId) {
+            this.companyId = companyId;
+        }
+
+        public List<EmployeeId> getEmployeeId() {
+            return employeeId;
+        }
+
+        public void setEmployeeId(List<EmployeeId> employeeId) {
+            this.employeeId = employeeId;
+        }
+
+        public String getStatus() {
+            return status;
+        }
+
+        public void setStatus(String status) {
+            this.status = status;
+        }
+
+        public String getBillUrl() {
+            return billUrl;
+        }
+
+        public void setBillUrl(String billUrl) {
+            this.billUrl = billUrl;
+        }
+
+        public static class EmployeeId {
+
+            @SerializedName("id")
+            @Expose
+            private Integer id;
+            @SerializedName("employeeID")
+            @Expose
+            private String employeeID;
+            @SerializedName("company_id")
+            @Expose
+            private Integer companyId;
+            @SerializedName("full_name")
+            @Expose
+            private String fullName;
+            @SerializedName("email")
+            @Expose
+            private String email;
+            @SerializedName("new_password")
+            @Expose
+            private String newPassword;
+            @SerializedName("gender")
+            @Expose
+            private String gender;
+            @SerializedName("father_name")
+            @Expose
+            private String fatherName;
+            @SerializedName("mobile_number")
+            @Expose
+            private String mobileNumber;
+            @SerializedName("date_of_birth")
+            @Expose
+            private String dateOfBirth;
+            @SerializedName("department_id")
+            @Expose
+            private String departmentId;
+            @SerializedName("designation")
+            @Expose
+            private Integer designation;
+            @SerializedName("joining_date")
+            @Expose
+            private String joiningDate;
+            @SerializedName("profile_image")
+            @Expose
+            private String profileImage;
+            @SerializedName("local_address")
+            @Expose
+            private String localAddress;
+            @SerializedName("permanent_address")
+            @Expose
+            private String permanentAddress;
+            @SerializedName("medical_leave")
+            @Expose
+            private String medicalLeave;
+            @SerializedName("annual_leave")
+            @Expose
+            private String annualLeave;
+            @SerializedName("status")
+            @Expose
+            private String status;
+            @SerializedName("last_login")
+            @Expose
+            private String lastLogin;
+            @SerializedName("remember_token")
+            @Expose
+            private String rememberToken;
+            @SerializedName("exit_date")
+            @Expose
+            private String exitDate;
+            @SerializedName("first_over_time_start")
+            @Expose
+            private String firstOverTimeStart;
+            @SerializedName("first_over_time_end")
+            @Expose
+            private String firstOverTimeEnd;
+            @SerializedName("first_over_time_amount")
+            @Expose
+            private String firstOverTimeAmount;
+            @SerializedName("second_over_time_start")
+            @Expose
+            private String secondOverTimeStart;
+            @SerializedName("second_over_time_end")
+            @Expose
+            private String secondOverTimeEnd;
+            @SerializedName("second_over_time_amount")
+            @Expose
+            private String secondOverTimeAmount;
+            @SerializedName("reset_code")
+            @Expose
+            private String resetCode;
+            @SerializedName("created_at")
+            @Expose
+            private String createdAt;
+            @SerializedName("updated_at")
+            @Expose
+            private String updatedAt;
+            @SerializedName("profile_image_url")
+            @Expose
+            private String profileImageUrl;
+            @SerializedName("work_duration")
+            @Expose
+            private String workDuration;
+
+            public Integer getId() {
+                return id;
+            }
+
+            public void setId(Integer id) {
+                this.id = id;
+            }
+
+            public String getEmployeeID() {
+                return employeeID;
+            }
+
+            public void setEmployeeID(String employeeID) {
+                this.employeeID = employeeID;
+            }
+
+            public Integer getCompanyId() {
+                return companyId;
+            }
+
+            public void setCompanyId(Integer companyId) {
+                this.companyId = companyId;
+            }
+
+            public String getFullName() {
+                return fullName;
+            }
+
+            public void setFullName(String fullName) {
+                this.fullName = fullName;
+            }
+
+            public String getEmail() {
+                return email;
+            }
+
+            public void setEmail(String email) {
+                this.email = email;
+            }
+
+            public String getNewPassword() {
+                return newPassword;
+            }
+
+            public void setNewPassword(String newPassword) {
+                this.newPassword = newPassword;
+            }
+
+            public String getGender() {
+                return gender;
+            }
+
+            public void setGender(String gender) {
+                this.gender = gender;
+            }
+
+            public String getFatherName() {
+                return fatherName;
+            }
+
+            public void setFatherName(String fatherName) {
+                this.fatherName = fatherName;
+            }
+
+            public String getMobileNumber() {
+                return mobileNumber;
+            }
+
+            public void setMobileNumber(String mobileNumber) {
+                this.mobileNumber = mobileNumber;
+            }
+
+            public String getDateOfBirth() {
+                return dateOfBirth;
+            }
+
+            public void setDateOfBirth(String dateOfBirth) {
+                this.dateOfBirth = dateOfBirth;
+            }
+
+            public String getDepartmentId() {
+                return departmentId;
+            }
+
+            public void setDepartmentId(String departmentId) {
+                this.departmentId = departmentId;
+            }
+
+            public Integer getDesignation() {
+                return designation;
+            }
+
+            public void setDesignation(Integer designation) {
+                this.designation = designation;
+            }
+
+            public String getJoiningDate() {
+                return joiningDate;
+            }
+
+            public void setJoiningDate(String joiningDate) {
+                this.joiningDate = joiningDate;
+            }
+
+            public String getProfileImage() {
+                return profileImage;
+            }
+
+            public void setProfileImage(String profileImage) {
+                this.profileImage = profileImage;
+            }
+
+            public String getLocalAddress() {
+                return localAddress;
+            }
+
+            public void setLocalAddress(String localAddress) {
+                this.localAddress = localAddress;
+            }
+
+            public String getPermanentAddress() {
+                return permanentAddress;
+            }
+
+            public void setPermanentAddress(String permanentAddress) {
+                this.permanentAddress = permanentAddress;
+            }
+
+            public String getMedicalLeave() {
+                return medicalLeave;
+            }
+
+            public void setMedicalLeave(String medicalLeave) {
+                this.medicalLeave = medicalLeave;
+            }
+
+            public String getAnnualLeave() {
+                return annualLeave;
+            }
+
+            public void setAnnualLeave(String annualLeave) {
+                this.annualLeave = annualLeave;
+            }
+
+            public String getStatus() {
+                return status;
+            }
+
+            public void setStatus(String status) {
+                this.status = status;
+            }
+
+            public String getLastLogin() {
+                return lastLogin;
+            }
+
+            public void setLastLogin(String lastLogin) {
+                this.lastLogin = lastLogin;
+            }
+
+            public String getRememberToken() {
+                return rememberToken;
+            }
+
+            public void setRememberToken(String rememberToken) {
+                this.rememberToken = rememberToken;
+            }
+
+            public String getExitDate() {
+                return exitDate;
+            }
+
+            public void setExitDate(String exitDate) {
+                this.exitDate = exitDate;
+            }
+
+            public String getFirstOverTimeStart() {
+                return firstOverTimeStart;
+            }
+
+            public void setFirstOverTimeStart(String firstOverTimeStart) {
+                this.firstOverTimeStart = firstOverTimeStart;
+            }
+
+            public String getFirstOverTimeEnd() {
+                return firstOverTimeEnd;
+            }
+
+            public void setFirstOverTimeEnd(String firstOverTimeEnd) {
+                this.firstOverTimeEnd = firstOverTimeEnd;
+            }
+
+            public String getFirstOverTimeAmount() {
+                return firstOverTimeAmount;
+            }
+
+            public void setFirstOverTimeAmount(String firstOverTimeAmount) {
+                this.firstOverTimeAmount = firstOverTimeAmount;
+            }
+
+            public String getSecondOverTimeStart() {
+                return secondOverTimeStart;
+            }
+
+            public void setSecondOverTimeStart(String secondOverTimeStart) {
+                this.secondOverTimeStart = secondOverTimeStart;
+            }
+
+            public String getSecondOverTimeEnd() {
+                return secondOverTimeEnd;
+            }
+
+            public void setSecondOverTimeEnd(String secondOverTimeEnd) {
+                this.secondOverTimeEnd = secondOverTimeEnd;
+            }
+
+            public String getSecondOverTimeAmount() {
+                return secondOverTimeAmount;
+            }
+
+            public void setSecondOverTimeAmount(String secondOverTimeAmount) {
+                this.secondOverTimeAmount = secondOverTimeAmount;
+            }
+
+            public String getResetCode() {
+                return resetCode;
+            }
+
+            public void setResetCode(String resetCode) {
+                this.resetCode = resetCode;
+            }
+
+            public String getCreatedAt() {
+                return createdAt;
+            }
+
+            public void setCreatedAt(String createdAt) {
+                this.createdAt = createdAt;
+            }
+
+            public String getUpdatedAt() {
+                return updatedAt;
+            }
+
+            public void setUpdatedAt(String updatedAt) {
+                this.updatedAt = updatedAt;
+            }
+
+            public String getProfileImageUrl() {
+                return profileImageUrl;
+            }
+
+            public void setProfileImageUrl(String profileImageUrl) {
+                this.profileImageUrl = profileImageUrl;
+            }
+
+            public String getWorkDuration() {
+                return workDuration;
+            }
+
+            public void setWorkDuration(String workDuration) {
+                this.workDuration = workDuration;
+            }
+
+        }
+    }
+}
